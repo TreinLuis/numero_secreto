49c6cb9c7789fd3d303fd178f8c40594cf0facc6
TreinLuis <21luiseduardo2005@gmail.com>
2024-05-25 22:42:47 -0300

    removendo linh a

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';
 }
-
 function reiniciarJogo() {
     numeroSecreto = gerarNumeroAleatorio();
     limparCampo();
